d085df59c33aef0b3ce75e05c7437fa8c0d1c645
Danish Bhatia <danishbhatia58@gmail.com>
2017-07-20 11:11:03 +0530

    finally data saved from signup credentials

--- mydjangoproject/mydjangoproject/views.py
+++ mydjangoproject/mydjangoproject/views.py
@@ -2,13 +2,31 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from datetime import datetime
+#from datetime import datetime
 from demoapp.forms import SignUpForm
+from django.contrib.auth.hashers import make_password   # to encrypt the pswrd
+from demoapp.models import UserModel
 
 # Create your views here.
 def signup_view(request):
-    today = datetime.now
-    signup_form = SignUpForm()
-    return render(request,'signup.html',{'signup_form':signup_form})
+    if request.method == 'GET':
+        #display signup form
+        signup_form = SignUpForm()
     
+    elif request.method == 'POST': 
+        #process the form data
+        signup_form = SignUpForm(request.POST)
+        
+        #validate the form data
+        if signup_form.is_valid():
+            #validation sucessfull
+            username = signup_form.cleaned_data['username']
+            name     = signup_form.cleaned_data['name']
+            email    = signup_form.cleaned_data['email']
+            password = signup_form.cleaned_data['password']
+            
+            #save data to db
+            new_user = UserModel(name = name, email = email,password = make_password(password), username = username)
+            new_user.save()    
 
+    return render(request,'signup.html',{'signup_form':signup_form})
